5c4eaff95b011f1d23537cc7f9cba1bb9e5a6e82
Григорий Слоква <Tyuiu.SlokvaGA@outlook.com>
2025-11-08 11:26:06 +0500

    Task6V15

---
+++ Tyuiu.SlokvaGA.Sprint4.Task6.V15.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.SlokvaGA.Sprint4.Task6.V15.Lib;
+
+namespace Tyuiu.SlokvaGA.Sprint4.Task6.V15.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+            var cities = new string[] { "Чикаго", "Хьюстон", "Феникс", "Филадельфия", "Сан-Антонио", "Сан-Диего", "Даллас" };
+            int res = ds.Calculate(cities);
+            int wait = 3;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.SlokvaGA.Sprint4.Task6.V15/Program.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.SlokvaGA.Sprint4.Task6.V15.Lib;
+
+namespace Tyuiu.SlokvaGA.Sprint4.Task6.V15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнил: Слоква Г. А. | ИСПб-25-1";
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спринт #4                                                              *");
+            Console.WriteLine("* Тема: Класс Array                                                      *");
+            Console.WriteLine("* Задание #6                                                             *");
+            Console.WriteLine("* Вариант #15                                                            *");
+            Console.WriteLine("* Выполнил: Слоква Г. А. | ИСПб-25-1                                     *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("* Дан строковый массив данных ['Чикаго', 'Хьюстон', 'Феникс',            *");
+            Console.WriteLine("* 'Филадельфия', 'Сан-Антонио', 'Сан-Диего', 'Даллас'] используя класс   *");
+            Console.WriteLine("* Array подсчитайте количество элементов, длина которых меньше 7.        *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+
+            var cities = new string[] { "Чикаго", "Хьюстон", "Феникс", "Филадельфия", "Сан-Антонио", "Сан-Диего", "Даллас" };
+            Console.WriteLine("Исходный массив:");
+            for (int i = 0; i <= cities.Length - 1; i++)
+            {
+                Console.WriteLine(cities[i]);
+            }
+
+            Console.WriteLine("************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                               *");
+            Console.WriteLine("************************************************************");
+            Console.WriteLine("Количество элементов, длина которых < 7:");
+
+            int n = ds.Calculate(cities);
+
+            Console.WriteLine(n);
+            Console.ReadKey();
+        }
+    }
+}
